7737cea85c618660a421e1916f836bc452f059fa
SuminAnn <annsumin2066@naver.com>
2023-01-31 10:51:27 +0900

    Intent 화면 전환 및 putExtra(data 전달)

--- app/src/main/java/com/example/myapplication/EditTextActivity.java
+++ app/src/main/java/com/example/myapplication/EditTextActivity.java
@@ -1,11 +1,12 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class EditTextActivity extends AppCompatActivity {
 
@@ -15,7 +16,10 @@ public class EditTextActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.edit_text);
+        setContentView(R.layout.activity_edit_text);
+
+        Intent intent = getIntent(); //intent로 넘어온 값을 받는다
+        String str = intent.getStringExtra("str");
 
         et_id = findViewById(R.id.et_id);
         btn_test = findViewById(R.id.btn_test);
@@ -23,7 +27,7 @@ public class EditTextActivity extends AppCompatActivity {
         btn_test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                et_id.setText("Button Click");
+                et_id.setText(str);
             }
         });
     }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,10 +13,28 @@ import android.widget.Toast;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_move;
+    private EditText et_text;
+    private  String str;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_text = findViewById(R.id.et_text);
+
+        btn_move = findViewById(R.id.btn_move);
+
+        btn_move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                str = et_text.getText().toString(); //editText의 값을 String으로 변환
+                Intent intent = new Intent(MainActivity.this, EditTextActivity.class); //(기존화면, 이동하고싶은 화면)
+                intent.putExtra("str",str); //putExtra(data를 담는다)
+                startActivity(intent); // 액티비티 이동
+            }
+        });
     }
 
 
